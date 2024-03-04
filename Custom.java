41720da6d35a74c45959d480c93953b6cd6c3e26
VerygoodMuhriwa <verygoodmuhriwa2@gmail.com>
2024-03-04 22:00:46 +0200

    1709582445992

--- Custom.java
+++ Custom.java
@@ -21,7 +21,7 @@ public class Custom  extends Exception{
             e.printStackTrace();
 
         }finally{
-          System.out.println("")
+          System.out.println("The try catch block has alr")
         }
     }
 }
